ee729a41f02386f13eeffc5c6583d5db505e7a21
jdcheek <58921502+jdcheek@users.noreply.github.com>
2022-09-30 22:31:02 -0500

    begining of using match

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");    
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is {secret_number}");
+
     println!("Please input your guess.");
+
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line...");
 
+    if guess == secret_number {
+        println!("You guessed correctly!");
+    } else {
+        println!("Wrong number, maybe next time...");
+    }
     println!("You guessed: {guess}");
 }
