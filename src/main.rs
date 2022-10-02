506ec251716a00dfca991606229a1149b74027cc
jdcheek <58921502+jdcheek@users.noreply.github.com>
2022-10-02 13:04:37 -0500

    initialized shadow of guess as u32 int, created match case for guess. basic game functions now

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering; 
 
 fn main() {
     println!("Guess the number!");    
@@ -16,10 +17,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line...");
 
-    if guess == secret_number {
-        println!("You guessed correctly!");
-    } else {
-        println!("Wrong number, maybe next time...");
-    }
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+    
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
